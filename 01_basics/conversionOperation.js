ff35fac354c4ce239c7ad774ef31990513f47fa8
Bharat Dixit <162457187+ErBharatdixit@users.noreply.github.com>
2025-01-13 07:55:35 +0000

    Conversion of dataTypes

---
+++ 01_basics/conversionOperation.js
@@ -0,0 +1,25 @@
+let score = "33adc"
+
+console.log(typeof score); // string
+
+let valueInNumber = Number(score)
+
+console.log(typeof valueInNumber ); // nuymber
+console.log( valueInNumber ); // Nan
+// "33" => 33
+// "33abc" => Nan
+// true => 1; false => 0
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+// 1 => true, 0 => false
+// " " => false
+// "bharat" = => true
+
+let someNumber = 13
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log( typeof stringNumber);
+
+
