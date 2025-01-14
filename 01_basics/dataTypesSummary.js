9044347456848ea0f1691ccb8f39e17bde47be63
Bharat Dixit <162457187+ErBharatdixit@users.noreply.github.com>
2025-01-14 06:31:07 +0000

    DataTypes summary

---
+++ 01_basics/dataTypesSummary.js
@@ -0,0 +1,35 @@
+// primitive 
+
+// 7 types : string,Number,null,Boolean,undefined,Symbol,BigInt
+
+const score = 100
+const scorevalue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+
+let userEmail; // undefined
+
+const id = Symbol('123')
+
+const anotherId = Symbol('123')
+console.log( id === anotherId);
+
+
+const bigNumber = 12123232323434n // using BigInt
+
+const heros = ["shakti" , "akshay", "mahesh"]
+ let myObj ={
+    name: "bharat",
+    age: 22,
+}
+const myfunction = function(){
+    console.log("hello world");
+    
+}
+console.log(typeof myfunction);
+
+
+// Refrence (Non primitive)
+
+// Array, Objects, functions
